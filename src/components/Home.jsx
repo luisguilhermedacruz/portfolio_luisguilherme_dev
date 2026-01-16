d947857cd0314b41768bffdb959cbd6af8335358
Luis Guilherme da Cruz <guicruz95@hotmail.com>
2026-01-16 19:30:36 -0300

    Alterações na home e inclusão da tradução em algumas partes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,28 @@
 import { useTranslation } from "react-i18next";
-import style from '../styles/Home.module.css';
-import backgroundImage from '../assets/fundo_1.jpg';
-import PerfilImage from '../assets/perfil.jpg';
+import style from "../styles/Home.module.css";
+import backgroundImage from "../assets/fundo_1.jpg";
+import PerfilImage from "../assets/perfil.jpg";
 
 const Home = () => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   return (
-    <div className={style.home_container}
-      style={{ backgroundImage: `url(${backgroundImage})`}}
+    <div
+      className={style.home_container}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <h1 className={style.welcome_text}>{t('home.welcome')}</h1>
-      <img src={PerfilImage} alt="Descrição da Imagem de Perfil" className={style.imagem_de_perfil} />
+      <div className={style.welcome_text_container}>
+        <h1 className={style.welcome_text}>{t("home.welcome")}</h1>
+        <h1 className={style.welcome_text_2}>{t("home.welcome2")}</h1>
+      </div>
+      <img
+        src={PerfilImage}
+        alt={t("home.description")}
+        className={style.profile_picture}
+      />
+      <h1 className={style.about_text}>{t("home.about")}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
