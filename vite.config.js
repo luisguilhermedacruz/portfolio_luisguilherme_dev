d32a747d2b2f7027a79fa3912d6dd42eefe1d999
Luis Guilherme da Cruz <guicruz95@hotmail.com>
2026-03-14 08:19:57 -0300

    fix: I corrected the menu resolution for mobile phones.

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,15 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+const dirname = typeof dirname !== 'undefined' ? dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
+  // Adicione esta parte aqui:
+  server: {
+    host: true,
+  },
   test: {
     projects: [{
       extends: true,
